814622287c6ec4acb771c28690e08a00008a4870
Nicholas Nord <Nicholasnord@gmail.com>
2024-12-11 22:03:05 -0700

    Completed day6

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -10,7 +10,6 @@ for lines in contents:
     rows = len(contents)
 
 grid = [row.strip() for row in contents]
-print(contents)
 
 up = '^'
 left ='<'
@@ -30,6 +29,13 @@ def printAllNodes(nodes):
     for rows in nodes:
         for node in rows:   
             print("("+str(node.row) + " , " + str(node.col) + ')' + ': ' + node.letter + " , " + node.direction)
+
+def printGrid(nodes):
+    for rows in nodes:
+        line = ''
+        for node in rows:
+            line = line + node.letter
+        print(line)
 
 nodeGrid = [[0]*cols for _ in range(rows)]
 solutionPath = []
@@ -52,80 +58,11 @@ def nextStep(guardRow, guardCol):
     i = guardRow
     j = guardCol
 
-    # handle goard moving up
+    # handle guard moving up
     if nodeGrid[i][j].letter == up:
         # if we go off page, we are done
         if i - 1 < 0:
             return True, i, j
-        else:
-            # if we hit a '#', we rotate to right
-            if nodeGrid[i-1][j].letter == obstacle:
-                nodeGrid[i][j].letter = right
-                return False, i, j
-            else:
-                # otherwise, move the guard forward and change current letter to 'X'
-                nodeGrid[i][j].letter = path
-                nodeGrid[i][j].direction = up
-                nodeGrid[i - 1][j].letter = up
-                return False, i - 1, j
-    # handle goard moving right
-    elif nodeGrid[i][j].letter == right:
-        # if we go off page, we are done
-        if j + 1 >= cols:
-            return True, i, j
-        else:
-            # if we hit a '#', we rotate to guard's right, which is down
-            if nodeGrid[i][j + 1].letter == obstacle:
-                nodeGrid[i][j].letter = down
-                return False, i, j
-            else:
-                # otherwise, move the guard forward and change current letter to 'X'
-                nodeGrid[i][j].letter = path
-                nodeGrid[i][j].direction = right
-                nodeGrid[i][j + 1].letter = right
-                return False, i, j + 1
-    # handle goard moving down
-    elif nodeGrid[i][j].letter == down:
-        # if we go off page, we are done
-        if i + 1 >= rows:
-            return True, i, j
-        else:
-            # if we hit a '#', we rotate to guard's right, which is left
-            if nodeGrid[i + 1][j].letter == obstacle:
-                nodeGrid[i][j].letter = left
-                return False, i, j
-            else:
-                # otherwise, move the guard forward and change current letter to 'X'
-                nodeGrid[i][j].letter = path
-                nodeGrid[i][j].direction = down
-                nodeGrid[i + 1][j].letter = down
-                return False, i + 1, j 
-    # handle goard moving left
-    elif nodeGrid[i][j].letter == left:
-        # if we go off page, we are done
-        if j - 1 < 0:
-            return True, i, j
-        else:
-            # if we hit a '#', we rotate to guard's right, which is up
-            if nodeGrid[i][j - 1].letter == obstacle:
-                nodeGrid[i][j].letter = up
-                return False, i, j
-            else:
-                # otherwise, move the guard forward and change current letter to 'X'
-                nodeGrid[i][j].letter = path
-                nodeGrid[i][j].direction = left
-                nodeGrid[i][j - 1].letter = left
-                return False, i, j - 1
-            
-def nextStepWithAntiLoopTechnology(guardRow, guardCol):
-    i = guardRow
-    j = guardCol
-
-    # handle goard moving up
-    if nodeGrid[i][j].letter == up:
-        # if we go off page, we are done
-        if i - 1 < 0:
-            return True, i, j, False
         else:
             # if we hit a '#', we rotate to right
             if nodeGrid[i-1][j].letter == obstacle:
@@ -137,57 +74,63 @@ def nextStepWithAntiLoopTechnology(guardRow, guardCol):
                 nodeGrid[i][j].letter = path
                 nodeGrid[i][j].direction = up
                 nodeGrid[i - 1][j].letter = up
+                nodeGrid[i - 1][j].direction = up
                 return False, i - 1, j
-    # handle goard moving right
+    # handle guard moving right
     elif nodeGrid[i][j].letter == right:
         # if we go off page, we are done
         if j + 1 >= cols:
-            return True, i, j, False
+            return True, i, j
         else:
             # if we hit a '#', we rotate to guard's right, which is down
             if nodeGrid[i][j + 1].letter == obstacle:
                 nodeGrid[i][j].letter = down
+                nodeGrid[i][j].direction = down
                 return False, i, j
             else:
                 # otherwise, move the guard forward and change current letter to 'X'
                 nodeGrid[i][j].letter = path
                 nodeGrid[i][j].direction = right
                 nodeGrid[i][j + 1].letter = right
+                nodeGrid[i][j + 1].direction = right
                 return False, i, j + 1
-    # handle goard moving down
+    # handle guard moving down
     elif nodeGrid[i][j].letter == down:
         # if we go off page, we are done
         if i + 1 >= rows:
-            return True, i, j, False
+            return True, i, j
         else:
             # if we hit a '#', we rotate to guard's right, which is left
             if nodeGrid[i + 1][j].letter == obstacle:
                 nodeGrid[i][j].letter = left
+                nodeGrid[i][j].direction = left
                 return False, i, j
             else:
                 # otherwise, move the guard forward and change current letter to 'X'
                 nodeGrid[i][j].letter = path
                 nodeGrid[i][j].direction = down
                 nodeGrid[i + 1][j].letter = down
+                nodeGrid[i + 1][j].direction = down
                 return False, i + 1, j 
-    # handle goard moving left
+    # handle guard moving left
     elif nodeGrid[i][j].letter == left:
         # if we go off page, we are done
         if j - 1 < 0:
-            return True, i, j, False
+            return True, i, j
         else:
             # if we hit a '#', we rotate to guard's right, which is up
             if nodeGrid[i][j - 1].letter == obstacle:
                 nodeGrid[i][j].letter = up
+                nodeGrid[i][j].direction = up
                 return False, i, j
             else:
                 # otherwise, move the guard forward and change current letter to 'X'
                 nodeGrid[i][j].letter = path
                 nodeGrid[i][j].direction = left
                 nodeGrid[i][j - 1].letter = left
+                nodeGrid[i][j - 1].direction = left
                 return False, i, j - 1
             
-
 def partOne():
     startingRow, startingCol, _ = buildNodeGrid()
     sum = 0
@@ -207,31 +150,32 @@ def partOne():
 
 
 def partTwo():
-    startingRow, startingCol, startingLetter = buildNodeGrid()
-
+    sum = 0
     # Foreach position in solutionPath; place an obstacle letter
     for block in solutionPath:
+        startingRow, startingCol, startingLetter = buildNodeGrid()
         blockRow = block.row
         blockCol = block.col
 
         nodeGrid[blockRow][blockCol].letter = obstacle
-        nodeGrid[startingRow][startingCol] = startingLetter
+        nodeGrid[startingRow][startingCol].letter = startingLetter
 
         # nodeGrid is updated with potential block. Step through and see if we run into loop
-        sum = 0
         finished = False
         nextRow = startingRow
         nextCol = startingCol
-        looped = False
+        loopDetector = {}
         while(not finished):
-            finished, i, j, isLoop  = nextStepWithAntiLoopTechnology(nextRow, nextCol)
+            finished, i, j  = nextStep(nextRow, nextCol)
+            loopKey = (i, j, nodeGrid[i][j].direction)
+            if not finished and loopKey in loopDetector:
+                sum = sum + 1
+                break
+            loopDetector[loopKey] = True
             nextRow = i
             nextCol = j
-            looped = isLoop
-        if looped:
-            sum = sum + 1
 
-        return sum
+    return sum
 
 
 print('---')
